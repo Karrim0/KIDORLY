94f44d74773f5e6d538c9e4c189d81356d119334
karrim0 <karimhnfy1@gmail.com>
2026-04-19 23:57:52 +0200

    fix use client order in admin media page

--- src/app/[locale]/(admin)/admin/media/page.tsx
+++ src/app/[locale]/(admin)/admin/media/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-"use client";
 
+"use client";
+export const dynamic = "force-dynamic";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
